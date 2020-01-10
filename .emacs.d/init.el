bd2acc4b77df25d72160c2e1e6605e895d982aa0
Jimmy <talyian@gmail.com>
2020-01-10 11:31:00 -0500

    allow shell and C in org-babel

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t) (C . t))))
  '(package-archives
    (quote
 	(("gnu" . "https://elpa.gnu.org/packages/")
