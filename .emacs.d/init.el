bee1293c2399e52b48fb030c545198f8b9b4d066
Jimmy <talyian@gmail.com>
2020-01-21 10:48:33 -0500

    .

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("/mnt/c/Users/49422/git/notebook/logs.org")))
  '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t) (C . t))))
  '(package-archives
    (quote
@@ -19,7 +20,7 @@
 	 ("melpa" . "https://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-	(flx swiper-helm ivy flycheck company-lsp counsel evil evil-magit evil-org general lsp-mode lsp-ui magit projectile swiper))))
+	(flx swiper-helm flycheck company-lsp counsel evil evil-magit evil-org general lsp-mode lsp-ui magit projectile swiper))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
