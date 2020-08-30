dff26983aee7c014d5cf1ff2d465d747e22d706c
Jimmy <talyian@gmail.com>
2020-08-30 12:49:40 -0400

    add edits to wombat theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,20 +13,36 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (quote (("." . ,temporary-file-directory))))
  '(c-basic-offset 2)
- '(c-offsets-alist (quote ((arglist-intro . +) (arglist-close . 0))))
+ '(c-offsets-alist (quote ((brace-list-intro . +) (arglist-intro . +))))
+ '(custom-enabled-themes (quote (wombat-no-bold)))
+ '(custom-safe-themes
+   (quote
+    ("66317e35f4a81b05ebe0c665309ec15b44f2f23dc8c865563415da816414b86f" "89bd0c3d7941b5a49ae21732a8734ee0d9ade35a5b6bc9835588d9f85b95890c" default)))
+ '(flycheck-clang-language-standard "c++17")
  '(indent-tabs-mode nil)
+ '(lsp-clients-clangd-args (quote ("--compile-commands-dir=build")))
+ '(lsp-clients-clangd-executable "/usr/bin/clangd-10")
  '(lsp-enable-indentation nil)
  '(lsp-enable-on-type-formatting nil)
- '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t) (C . t))))
+ '(org-agenda-files (quote ("~/git/coral/src-cpp/tasks.org")))
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t) (C . t) (sqlite . t))))
+ '(org-hierarchical-todo-statistics nil)
+ '(org-html-checkbox-type (quote unicode))
+ '(org-html-creator-string "")
+ '(org-html-head-include-default-style nil)
+ '(org-html-html5-fancy t)
+ '(org-html-htmlize-output-type (quote css))
+ '(org-html-postamble nil)
+ '(org-html-validation-link "")
+ '(org-todo-keywords (quote ((sequence "TODO(t)" "DOING(i)" "|" "DONE(d!)"))))
  '(package-archives
    (quote
     (("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (lua-mode glsl-mode haskell-mode cmake-mode fsharp-mode flycheck company-lsp counsel evil evil-magit evil-org general lsp-mode lsp-ui magit projectile swiper swiper-helm flx)))
+    (yasnippet company lsp-ui csv-mode org-pomodoro rust-mode htmlize lua-mode glsl-mode haskell-mode cmake-mode fsharp-mode flycheck counsel evil evil-magit evil-org general magit projectile swiper swiper-helm flx)))
  '(python-shell-interpreter "python3")
  '(safe-local-variable-values
    (quote
--- .emacs.d/talyian.el
+++ .emacs.d/talyian.el
@@ -1,4 +1,5 @@
-;; General Editor setup 
+;; General Editor setup
+(xterm-mouse-mode)
 (evil-mode)
 (evil-magit-init)
 (tool-bar-mode -1)
@@ -9,6 +10,7 @@
     `((".*" . ,temporary-file-directory))
   auto-save-file-name-transforms
     `((".*" ,temporary-file-directory t)))
+(defalias 'yes-or-no-p 'y-or-n-p)
 ;; completions
 (ivy-mode)
 (setq ivy-use-virtual-buffers t)
@@ -18,33 +20,38 @@
    '((swiper . ivy--regex-plus)
 	 (t      . ivy--regex-fuzzy)))
 
+;; TODO: how do I set up general with lsp-command-map?
 ;; evil-mode leader key setup
 (general-evil-setup t)
 (general-evil-define-key 'normal 'global
   :prefix "SPC"
+  "bb" 'ibuffer
+  "cc" 'recompile
+  "ff" 'counsel-find-file
   "fs" 'save-buffer
   "fw" 'write-file
-  "ff" 'counsel-find-file
-  "pf" 'projectile-find-file
-  "ps" 'projectile-grep
-  "cc" 'recompile
-  "po" 'projectile-find-other-file
+  "oa" 'org-agenda
   "pc" 'recompile
+  "pf" 'projectile-find-file
+  "pg" 'projectile-grep
+  "po" 'projectile-find-other-file
+  "ps" 'projectile-grep
   )
 (general-evil-define-key 'normal 'global
   :prefix "g"
   "o" 'evil-jump-backward
   "i" 'evil-jump-forward)
-
 (global-set-key (kbd "<f5>") 'recompile)
 
 ;; clang settings
-(defvar talyian/clangd-exe (executable-find "clangd-8"))
-(defvar talyian/clang-exe (executable-find "clang-8"))
-(defvar talyian/clang++-exe (executable-find "clang++-8"))
+(defvar talyian/clangd-exe (executable-find "clangd-10"))
+(defvar talyian/clang-exe (executable-find "clang-10"))
+(defvar talyian/clang++-exe (executable-find "clang++-10"))
 (defun talyian/cpp-mode-init ()
   (company-mode)
   (lsp-deferred)
+  (yas-minor-mode)
+  (general-evil-define-key 'normal 'global :prefix "SPC" (kbd "l") lsp-command-map)
   (setq company-backends '(company-capf company-lsp company-keywords company-dabbrev))
   (setq lsp-clients-clangd-executable talyian/clangd-exe)
   (setq company-minimum-prefix-length 0)
@@ -53,5 +60,8 @@
 
 ;; coral mode
 (autoload 'coral-mode
-  (expand-file-name "coral-mode.el" (file-name-directory load-file-name))
+  (expand-file-name "coral-mode.el"
+					(condition-case nil
+						(file-name-directory load-file-name)
+					  (error ".")))
   "coral programming mode" t nil)
